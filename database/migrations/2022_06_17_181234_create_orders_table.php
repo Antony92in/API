99bee6c08122be0564dbf1d66b0ffcf66d204b9a
AntonV92 <av476347@gmail.com>
2022-06-17 21:26:46 +0300

    add create table migration

---
+++ database/migrations/2022_06_17_181234_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->char('full_name', 100);
+            $table->char('address', 200);
+            $table->double('amount', 8, 2);
+            $table->dateTime('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
